cd99d842f5a02144b4404df904334a5b48736c24
Kousaka Kazuma <kazu1107@gmail.com>
2018-06-11 17:25:09 +0900

    commit for the test project

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class Main : MonoBehaviour {
+
+    bool inited = false;
+    bool switching = false;
+
+    // Use this for initialization
+    void Start () {
+        XRSettings.enabled = false;
+    }
+
+	IEnumerator ToggleVR()
+	{
+        switching = true;
+
+		if (!inited ) {
+            inited = true;
+			XRSettings.LoadDeviceByName( "cardboard");
+        }
+        yield return null;
+        XRSettings.enabled = !XRSettings.enabled;
+        yield return null;
+        switching = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        foreach (var t in Input.touches){
+            
+            if ( t.phase == TouchPhase.Began && !switching){
+                Debug.Log("touch");
+                StartCoroutine(ToggleVR());
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    Quaternion r;
+
+    // Use this for initialization
+    void Start () {
+        r = Quaternion.Euler(1.5f, 2.5f, 3.5f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.localRotation *= r;
+    }
+}
